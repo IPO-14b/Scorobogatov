bce38b6e9e980c082fde0e6b41aecfb44d542a86
Skorobogatov1 <valscor@rambler.ru>
2017-05-25 23:27:05 +0300

    js

---
+++ main.js
@@ -0,0 +1,17 @@
+var ms_sux=false;
+var disable_ext=false;
+var killed=0;
+var missed=0;
+var total=3;
+var total_shoots=0;
+var timeleft=120;
+var reloaded=true;
+var lets_start=false;
+var rrr=7;
+function get_version(s){
+        result="";
+        for(i=0;i!=s.length;i++)
+                if((s.charAt(i)>="0")&&(s.charAt(i)<="9"))result+=s.charAt(i);
+                else break;
+        return result;
+}
