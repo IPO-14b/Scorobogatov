40625d1e8ad79763d752fa57966a20ff9e2512e0
Skorobogatov1 <valscor@rambler.ru>
2017-05-25 23:34:56 +0300

    js

--- main.js
+++ main.js
@@ -15,3 +15,11 @@ function get_version(s){
                 else break;
         return result;
 }
+if((navigator.appName=="Microsoft Internet Explorer")&&(get_version(navigator.appVersion)<4)){
+        disable_ext=true;
+        ms_sux=true;
+}
+
+if((navigator.appName=="Netscape")&&(get_version(navigator.appVersion)<3)){
+        disable_ext=true;
+}
