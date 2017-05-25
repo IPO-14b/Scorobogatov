af50da998e939bf9230ef7ba9a439155cea54e96
Skorobogatov1 <valscor@rambler.ru>
2017-05-25 23:40:24 +0300

    js

--- main.js
+++ main.js
@@ -1,25 +1,30 @@
-var ms_sux=false;
-var disable_ext=false;
-var killed=0;
-var missed=0;
-var total=3;
-var total_shoots=0;
-var timeleft=120;
-var reloaded=true;
-var lets_start=false;
-var rrr=7;
-function get_version(s){
-        result="";
-        for(i=0;i!=s.length;i++)
-                if((s.charAt(i)>="0")&&(s.charAt(i)<="9"))result+=s.charAt(i);
+var ms_sux = false;
+var disable_ext = false;
+var killed = 0;
+var missed = 0;
+var total = 3;
+var total_shoots = 0;
+var timeleft = 120;
+var reloaded = true;
+var lets_start = false;
+var rrr = 7;
+function get_version(s) {
+    result="";
+    for(i=0;i!=s.length;i++)
+        if((s.charAt(i)>="0")&&(s.charAt(i)<="9"))result+=s.charAt(i);
                 else break;
         return result;
 }
-if((navigator.appName=="Microsoft Internet Explorer")&&(get_version(navigator.appVersion)<4)){
+if((navigator.appName = ="Microsoft Internet Explorer")&&(get_version(navigator.appVersion)<4)){
         disable_ext=true;
         ms_sux=true;
 }
 
-if((navigator.appName=="Netscape")&&(get_version(navigator.appVersion)<3)){
+if((navigator.appName= ="Netscape")&&(get_version(navigator.appVersion)<3)){
         disable_ext=true;
+}
+function rndm(n) {
+        dt=new Date();
+        rrr=((Math.round(dt.getTime()/10)+rrr)%n);
+        return rrr;
 }
